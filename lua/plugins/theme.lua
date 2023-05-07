e1cf37b3324961cea0ad493c5e5f69a7a5751755
Aleks <am@klaut.org>
2023-05-07 23:28:07 +0200

    Update theme

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -3,8 +3,10 @@ return {
     "shaunsingh/nord.nvim",
     priority = 1000,
     init = function()
-      vim.g.nord_italic = false
+      vim.g.nord_contrast = true
       vim.g.nord_borders = true
+      vim.g.nord_italic = false
+
       vim.cmd[[colorscheme nord]]
     end,
   }
