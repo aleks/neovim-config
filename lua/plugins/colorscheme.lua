02af5dbb854ad7758bd8ddf4dd44691d439c04de
Aleks Maksimow <aleksander.maksimow@contentful.com>
2023-05-08 17:07:21 +0200

    Fix: Disable semantic tokens to fix nord.vim theme when LSP is used
    Fix semantic tokens breaking nord.vim

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -9,6 +9,21 @@ return {
       vim.g.nord_italic = false
 
       vim.cmd[[colorscheme nord]]
+
+      --
+      -- TODO: Remove this again once fixed!
+      -- turns off LSP semantic tokens by default
+      -- See: https://github.com/shaunsingh/nord.nvim/issues/143
+      --
+      vim.api.nvim_create_autocmd(
+        "LspAttach",
+        {
+          callback = function(args)
+            local client = vim.lsp.get_client_by_id(args.data.client_id)
+            client.server_capabilities.semanticTokensProvider = nil
+          end,
+        }
+      )
     end,
   }
 }
